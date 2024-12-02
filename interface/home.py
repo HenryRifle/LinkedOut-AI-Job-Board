042f74cf6796c1c03cc02ab9044513aab2d6aa28
HenryRilfe <105322150+HenryRifle@users.noreply.github.com>
2024-12-02 10:18:33 -0500

    Add files via upload

---
+++ interface/home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+
+
+
+def home():
+    name = st.session_state.username
+    st.title(f"Welcome to LinkedOut, {name} !")
+
+    st.subheader("Recommended for you: ")
+    
